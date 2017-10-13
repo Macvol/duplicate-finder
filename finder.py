302c26c2b9532dabf71684e143d0f0f508816c44
Gleb Kolobaev <kgv183@gmail.com>
2017-10-13 16:41:02 +0300

    Output size of processed files

--- finder.py
+++ finder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, hashlib, re
+import sys, os, hashlib, re, argparse
 
 ignore_hidden = False
 ignore_pattern = ''
@@ -13,28 +13,50 @@ file_list = list()
 file_ignored = list()
 file_missed = list()
 file_keys = dict()
+full_size = 0
 
 sys.stderr.write("Getting file list ...")
 sys.stderr.flush()
 
+# Check all files.
 for root, dir_names, file_names in os.walk(sys.argv[1]):
   for file_name in file_names:
+    # Get full file name
     full_file_name = os.path.join(root, file_name)
-    if ignore_hidden == True:
-      if re.match(ignore_pattern, full_file_name):
-        file_ignored.append(full_file_name)
-      else:
-        file_list.append(full_file_name)
-    else:
-      file_list.append(full_file_name)
+
+    # Check real file
+    if os.path.isfile(full_file_name):
+
+      # Getting filesize
+      try:
+        file_size = os.path.getsize(full_file_name)
+
+        # Dont process ignored files
+        if ignore_hidden == True:
+          if re.match(ignore_pattern, full_file_name):
+            file_ignored.append(full_file_name)
+          else:
+            file_list.append((full_file_name, file_size))
+            full_size += file_size
+        else:
+          file_list.append((full_file_name, file_size))
+          full_size += file_size
+
+      except:
+        file_missed.append(full_file_name)
 
 sys.stderr.write("\r                                     \r")
 sys.stderr.flush()
 
 file_count = 0
-for full_file_name in file_list:
-    file_count += 1
-    sys.stderr.write("\r%d/%d (%.0f%%)" % (file_count, len(file_list), 100 * float(file_count) / len(file_list)))
+size_count = 0
+for file_data in file_list:
+    full_file_name = file_data[0]
+    file_size = file_data[1]
+
+    sys.stderr.write("\r%d/%d (%.0f%%)\t%d/%d (%.0f%%)" %
+                     (file_count, len(file_list), 100. * float(file_count) / len(file_list),
+                      size_count, full_size, 100. * float(size_count) / full_size))
     sys.stderr.flush()
 
     #print full_file_name
@@ -48,17 +70,21 @@ for full_file_name in file_list:
     except:
       file_missed.append(full_file_name)
 
-sys.stderr.write("\r                                    \r")
-sys.stderr.flush()
+    file_count += 1
+    size_count += file_size
+
+    sys.stderr.write("\r                                                     \r")
+    sys.stderr.flush()
 
 #if ignore_hidden == True:
 #  print "Ignored:"
 #  for file_name in file_ignored:
 #    print "\t", file_name
 
-print "Missed:"
-for file_name in file_missed:
-  print "\t", file_name
+if len(file_missed) > 0:
+    print "Missed:"
+    for file_name in file_missed:
+      print "\t", file_name
 
 group_id = 1
 for file_key in file_keys:
